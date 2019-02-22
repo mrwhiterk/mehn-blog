8cb98fb34fc8511b0b3c22f5e78a379182251bab
Ryan White <mrwhiterk@gmail.com>
2019-02-22 17:12:23 -0500

    fixed port in index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const methodOverride = require("method-override")
 const bodyParser = require("body-parser")
 
 
-const port = 4000
+// const port = 4000
+// const port =  
 
 let HandlebarsIntl = require('handlebars-intl');
 HandlebarsIntl.registerWith(hbs);
@@ -22,7 +23,7 @@ app.use(require("./routes/blogPost.js"))
 
 app.set('port', process.env.PORT || 3001)
 
-app.listen(app.get(port), () => {
+app.listen(app.get('port'), () => {
   // console.log(`Server is up and running on ${app.get('port')}`)
   console.log(`✅ PORT: ${app.get('port')} 🌟`)
 })
