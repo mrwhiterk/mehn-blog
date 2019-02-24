155f9d226c4fec04019a423763a9672128e821a9
Ryan White <mrwhiterk@gmail.com>
2019-02-23 21:39:23 -0500

    added packages and index.js code for authentication

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const hbs = require("hbs")
 const methodOverride = require("method-override")
 const bodyParser = require("body-parser")
 
-
-// const port = 4000
-// const port =  
-
 let HandlebarsIntl = require('handlebars-intl');
 HandlebarsIntl.registerWith(hbs);
+
+//authenticate
+const cookieParser = require("cookie-parser")
+const flash = require("connect-flash")
+const session = require("express-session")
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
 
 app.set("view engine", "hbs")
 
@@ -19,11 +22,34 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('public'))
 app.use(methodOverride("_method"))
 
+// authenticate
+app.use(cookieParser())
+app.use(session({
+  secret: "Secret blog post",
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+const {
+  user
+} = require("./models/index")
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function (req, res, next) {
+  global.user = req.user;
+  res.locals.user = req.user;
+  next();
+})
+
 app.use(require("./routes/blogPost.js"))
 
 app.set('port', process.env.PORT || 3001)
 
 app.listen(app.get('port'), () => {
-  // console.log(`Server is up and running on ${app.get('port')}`)
   console.log(`✅ PORT: ${app.get('port')} 🌟`)
 })
